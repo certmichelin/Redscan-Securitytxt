04c9fca69a27e3e21aa2efbdfce6787251ff0d2d
Maxime Escourbiac <maxime.escourbiac@gmail.com>
2021-05-28 16:27:00 +0200

    Update

--- src/main/java/com/michelin/cert/redscan/SecuritytxtScanApplication.java
+++ src/main/java/com/michelin/cert/redscan/SecuritytxtScanApplication.java
@@ -9,6 +9,7 @@ import com.michelin.cert.redscan.utils.models.HttpService;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+
 import org.apache.logging.log4j.LogManager;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -29,7 +30,6 @@ public class SecuritytxtScanApplication {
   @Autowired
   private DatalakeConfig datalakeConfig;
 
-
   /**
    * RedScan Main methods.
    *
@@ -38,22 +38,7 @@ public class SecuritytxtScanApplication {
   public static void main(String[] args) {
     SpringApplication.run(SecuritytxtScanApplication.class, args);
   }
-   
-  /**
-   * Check that the file is at minima a security.txt.
-   * @param content is the content of the response body. Shloud be a securitytxt
-   * @return boolean with indicate if the file is possibly a security.txt
-   */
-  public boolean contentCheck(String content) {
-    //String[] unsigned_content = {"Contact:", "Encryption", "Policy", "Acknowledgments", "Expires"};
-    //String[] signed_content = {"Hash", "Canonical", "Contact", "Encryption", "Policy", "Acknowledgments", "Expires", "Version", "BEGIN PGP SINGATURE"};
-    
-    boolean valid = false;
-    if (content.contains("Contact:")) {
-      valid = true;
-    }
-    return valid;
-  }
+
   /**
    * Message executor.
    *
@@ -62,43 +47,44 @@ public class SecuritytxtScanApplication {
   @RabbitListener(queues = {RabbitMqConfig.QUEUE_HTTP_SERVICES})
   public void receiveMessage(String message) {
     HttpService serviceMessage = new HttpService(message);
-    LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Checking Security text on : %s", serviceMessage.getDomain()));
+    LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Checking Security text on : %s", serviceMessage.toUrl()));
     try {
-      String wellKnowUrl = String.format("%s/.well-known/security.txt", serviceMessage.toUrl());
-      String rootUrl = String.format("%s/security.txt", serviceMessage.toUrl());
-      LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("locating security.txt at %s", wellKnowUrl));
-      HttpResponse<String> response = Unirest.get(wellKnowUrl).asString();
-      if (response.getStatus() != 200) {
-        LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Cannot find security txt at %s %d",wellKnowUrl, response.getStatus()));
-        LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("locating security.txt at %s", rootUrl ));
-        response = Unirest.get(rootUrl).asString();
-        if (response.getStatus() != 200) {
-          LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Cannot find security txt at %s %d",rootUrl, response.getStatus()));
-        } else {
-          LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Found security.txt at %s", rootUrl));
-          if (contentCheck(response.getBody())) {
-            LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Upserting http service field with securitytxt content : %s", response.getBody()));
-            datalakeConfig.upsertHttpServiceField(serviceMessage.getDomain(), serviceMessage.getPort(), "securitytxt", response.getBody());
-          } else {
-            LogManager.getLogger(SecuritytxtScanApplication.class).warn(String.format("Security.txt has not passed content checker : %s", response.getBody()));
-          }
-          
-        }
-      } else {
-        LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Found security.txt at %s", wellKnowUrl));
-        if (contentCheck(response.getBody())) {
-          LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Upserting http service field with securitytxt content : %s", response.getBody()));
-          datalakeConfig.upsertHttpServiceField(serviceMessage.getDomain(), serviceMessage.getPort(), "securitytxt", response.getBody());
-        } else {
-          LogManager.getLogger(SecuritytxtScanApplication.class).warn(String.format("Security.txt has not passed content checker : %s", response.getBody()));
-        }
+      String url = String.format("%s/.well-known/security.txt", serviceMessage.toUrl());
+      String result = retrieveSecurityTxtContent(url);
+      if (result == null) {
+        url = String.format("%s/security.txt", serviceMessage.toUrl());
+        result = retrieveSecurityTxtContent(url);
       }
-            
+
+      if (result != null) {
+        LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Found security.txt at %s", url));
+        datalakeConfig.upsertHttpServiceField(serviceMessage.getDomain(), serviceMessage.getPort(), "securitytxt", result);
+      } else {
+        LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Security text not found %s", serviceMessage.toUrl()));
+        datalakeConfig.upsertHttpServiceField(serviceMessage.getDomain(), serviceMessage.getPort(), "securitytxt", "not found");
+      }
     } catch (DatalakeStorageException ex) {
-      LogManager.getLogger(SecuritytxtScanApplication.class).error(String.format("Exception with datalake : %s", ex));
-      
+      LogManager.getLogger(SecuritytxtScanApplication.class).error(String.format("Datalake Strorage exception : %s", ex));
+    } catch (Exception ex) {
+      LogManager.getLogger(SecuritytxtScanApplication.class).error(String.format("Exception : %s", ex));
+    } 
+  }
+
+  private String retrieveSecurityTxtContent(String url) {
+    String result = null;
+    LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("Checking Security txt on : %s", url));
+    HttpResponse<String> response = Unirest.get(url).asString();
+    if (response.getStatus() == 200) {
+      LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("URL (%s) return 200 response code", url));
+      if (response.getBody() != null && response.getBody().contains("Contact:")) {
+        result = response.getBody();
+      } else {
+        LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("URL (%s) seems not be a real security txt file", url));
+      }
+    } else {
+      LogManager.getLogger(SecuritytxtScanApplication.class).info(String.format("URL (%s) return %s response code", url, response.getStatus()));
     }
-    
+    return result;
   }
 
 }
